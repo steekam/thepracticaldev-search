e7d2a3c646a275f44b047e89d57e77c57006d21d
steekam <waynewanyee@gmail.com>
2021-01-08 23:14:11 +0300

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,15 +7,22 @@
         </div>
     </nav> --}}
 
-    <main class="container px-4 pt-24 mx-auto sm:px-6 lg:px-8">
-        <div class="max-w-4xl">
+    <main class="container px-4 mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-4xl mt-24">
             <h1 class="text-5xl font-bold leading-tight text-gray-900 capitalise">
                 Article search results rank based on comments' sentiment.
             <h1>
 
-            <p class="mt-8 text-xl leading-relaxed text-gray-700">
-                Think of StackOverflow but the upvote and downvote is on articles and is based on what readers are saying in the comments.
-            </p>
+            <div class="mt-8 space-y-4 text-lg text-justify text-gray-700 sm:text-2xl sm:leading-relaxed">
+                <p>
+                    Classifying the sentiment of readers' comments into positive or negative. The aggregate sentiment score of the articles helps ranking search results based on readers feedback. <strong>Helps you identify articles that are likely to be useful based what people are saying.</strong>
+                </p>
+
+                <p>
+                    Think of StackOverflow for articles but the up vote is a positive comment and the down vote is a negative comment.
+                </p>
+            </div>
+
         </div>
     </main>
 </x-layouts.base>
